18aa3adc9e2d182af4e9a20df6384d8f50299b95
victor rodrigues | tprime desktop <victor_duu@hotmail.com>
2025-10-27 14:38:18 -0300

    fix(app.module): corrige import do app.controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "./utils/prisma/prisma.service";
 import { UserModule } from "./user/user.module";
 import { AuthModule } from './auth/auth.module';
-import { AppController } from './app/app.controller';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [UserModule, AuthModule],
